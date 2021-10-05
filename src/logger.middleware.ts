c8b1144b61a9af998fad0b3b00568b80eabc0079
박준영 <sjwm98@gmail.com>
2021-10-06 01:47:45 +0900

    middleware 추가

---
+++ src/logger.middleware.ts
@@ -0,0 +1,19 @@
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  //로깅사용하기
+  private logger = new Logger('HTTP');
+  //미들웨어 만들기
+  use(req: Request, res: Response, next: NextFunction) {
+    res.on('finish', () => {
+      this.logger.log(
+        `${req.ip} ${req.method} ${res.statusCode}`,
+        req.originalUrl,
+      );
+    });
+
+    next();
+  }
+}
