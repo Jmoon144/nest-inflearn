9bc143d58942dda86e302eb7c0a23b9bc6a410a3
박준영 <sjwm98@gmail.com>
2021-10-06 01:28:31 +0900

    캡슐화 추가

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -4,6 +4,7 @@ import { CatsService } from './cats.service';
 
 @Module({
   controllers: [CatsController],
+  //provider 외부 사용 불가
   providers: [CatsService],
   //외부에서 사용할 수 있게 내보내기
   exports: [CatsService],
